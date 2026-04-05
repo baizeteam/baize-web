ae2bc24c56c137b35b0d19093b6ea5f03d6bed04
xiaoyu <xiaoyu@sulg.top>
2026-04-06 02:09:39 +0800

    ✨ 统一数据源

--- src/components/BaizeToolbox/BaizeToolboxInfo.tsx
+++ src/components/BaizeToolbox/BaizeToolboxInfo.tsx
@@ -4,14 +4,15 @@ import PageInfo from "../Common/PageInfo";
 import Download from "./download";
 import { gitHubUrl } from "@/utils/textHelper";
 
-export const data = {
-  title: "白泽工具箱",
-  description:
-    "白泽工具箱是一款功能强大的多媒体工具，为用户提供了多种多样的多媒体处理功能。无论您是需要对音视频转码、提取音频/视频/字幕、文字转语音(TTS)、屏幕录制、截图，还是对音视频图片进行压缩，这个工具都能轻松实现。",
-};
 export const dynamic = "force-dynamic";
 
-export default async function BaizeToolboxInfo() {
+export default async function BaizeToolboxInfo({
+  title,
+  description,
+}: {
+  title: any;
+  description: React.ReactNode;
+}) {
   async function init() {
     const [repoInfoRes, repoReleasesRes, contributorsRes] = await Promise.all([
       fetch(repoUrl, baseParams),
@@ -29,7 +30,7 @@ export default async function BaizeToolboxInfo() {
 
   return (
     <div className="container">
-      <PageInfo title={data?.title} description={data?.description} />
+      <PageInfo title={title} description={description} />
 
       {/* <AppGithubStatus repoInfo={repoInfo} contributors={contributors} /> */}
 
